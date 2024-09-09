6d8ffac26956bf8374997533b8e3e0333a2b60d2
Greg Barnes <gregdbarnes@gmail.com>
2024-09-09 13:15:02 +0100

    Removes unused CSV export, fixes line length issues

--- orp/core/forms.py
+++ orp/core/forms.py
@@ -21,12 +21,13 @@ class CookiePageConsentForm(forms.Form):
 class RegulationSearchForm(forms.Form):
     """Regulation Search Form.
 
-    Regulation search form for searching for regulations by name or description.
+    Regulation search form for searching
+    for regulations by name or description.
     """
 
     query = forms.CharField(
         required=False,
-        label="Enter the sector, business activity or topic you want to find regulations for.",
+        label="Enter the sector, business activity or topic you want to find regulations for.",  # noqa: E501
         help_text="For example, Construction, Health and Safety, or Food",
         widget=forms.TextInput(
             attrs={
