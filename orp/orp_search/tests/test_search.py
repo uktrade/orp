27b9ac9e1ba3b5a6519e023a2d591a2568bfa1ff
Haresh Kainth <haresh.kainth@businessandtrade.gov.uk>
2024-11-26 01:20:13 +0000

    chore:add tests for implicit AND operator in search queries
    Introduce unittests to verify implicit AND operator handling in `create_search_query` when parsing multiple terms. Ensure each term is queried individually, then combined using the AND operation.

--- orp/orp_search/tests/test_search.py
+++ orp/orp_search/tests/test_search.py
@@ -14,6 +14,54 @@ class TestCreateSearchQuery(unittest.TestCase):
         self.assertEqual(result, mock_search_query.return_value)
 
     @patch("orp_search.utils.search.SearchQuery", autospec=True)
+    def test_implicit_and_search_operator_query(self, mock_search_query):
+        # Mock SearchQuery instances
+        mock_query1 = MagicMock(name="MockQuery1")
+        mock_query2 = MagicMock(name="MockQuery2")
+
+        # Configure the mock to return mock objects
+        mock_search_query.side_effect = [mock_query1, mock_query2]
+
+        # Call the function
+        _ = create_search_query("test trial")
+
+        # Assert that SearchQuery was called with expected arguments
+        calls = [
+            call("test", search_type="plain"),
+            call("trial", search_type="plain"),
+        ]
+        mock_search_query.assert_has_calls(calls, any_order=False)
+
+        # Assert the AND operation was applied
+        mock_query1.__and__.assert_called_once_with(mock_query2)
+
+    @patch("orp_search.utils.search.SearchQuery", autospec=True)
+    def test_multiple_implicit_and_search_operator_query(
+        self, mock_search_query
+    ):
+        # Mock SearchQuery instances
+        mock_query1 = MagicMock(name="MockQuery1")
+        mock_query2 = MagicMock(name="MockQuery2")
+        mock_query3 = MagicMock(name="MockQuery3")
+
+        # Configure the mock to return mock objects
+        mock_search_query.side_effect = [mock_query1, mock_query2, mock_query3]
+
+        # Call the function
+        _ = create_search_query("test trial error")
+
+        # Assert that SearchQuery was called with expected arguments
+        calls = [
+            call("test", search_type="plain"),
+            call("trial", search_type="plain"),
+            call("error", search_type="plain"),
+        ]
+        mock_search_query.assert_has_calls(calls, any_order=False)
+
+        # Assert the AND operation was applied
+        mock_query1.__and__.assert_called_with(mock_query3)
+
+    @patch("orp_search.utils.search.SearchQuery", autospec=True)
     def test_and_search_operator_query(self, mock_search_query):
         # Mock SearchQuery instances
         mock_query1 = MagicMock(name="MockQuery1")
