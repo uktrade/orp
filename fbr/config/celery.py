839cb0c0cd7a4205f59ca91aa6d82a0128585442
Haresh Kainth <haresh.kainth@businessandtrade.gov.uk>
2024-12-04 15:11:07 +0000

    chore: fix variable path

--- fbr/config/celery.py
+++ fbr/config/celery.py
@@ -7,9 +7,7 @@ from dbt_copilot_python.celery_health_check import healthcheck
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 celery_app = Celery("fbr_celery")
-celery_app.config_from_object(
-    "django.conf:config.settings.local", namespace="CELERY"
-)
+celery_app.config_from_object("django.conf:settings.local", namespace="CELERY")
 celery_app.autodiscover_tasks()
 
 celery_app = healthcheck.setup(celery_app)
