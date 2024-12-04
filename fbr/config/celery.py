7c6f8e3f376a7ece0c686c81e9615c01ffe3cf6f
Haresh Kainth <haresh.kainth@businessandtrade.gov.uk>
2024-12-04 14:41:12 +0000

    chore: renamed config path

--- fbr/config/celery.py
+++ fbr/config/celery.py
@@ -4,11 +4,11 @@ from celery import Celery
 from celery.schedules import crontab
 from dbt_copilot_python.celery_health_check import healthcheck
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fbr.config.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 celery_app = Celery("fbr_celery")
 celery_app.config_from_object(
-    "django.conf:fbr.config.settings.local", namespace="CELERY"
+    "django.conf:config.settings.local", namespace="CELERY"
 )
 celery_app.autodiscover_tasks()
 
@@ -16,7 +16,7 @@ celery_app = healthcheck.setup(celery_app)
 
 celery_app.conf.beat_schedule = {
     "schedule-fbr-cache-task": {
-        "task": "fbr.cache.tasks.rebuild_cache",
+        "task": "cache.tasks.rebuild_cache",
         "schedule": crontab(hour="1", minute="0"),
     },
 }
