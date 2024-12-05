fd857ceadaa3311ce82132822e6bd4b67dbcc94b
Haresh Kainth <haresh.kainth@businessandtrade.gov.uk>
2024-12-05 14:48:19 +0000

    chore: set Django settings module for Celery configuration.
    This change imports 'os' and sets the 'DJANGO_SETTINGS_MODULE' environment variable to use local settings for Celery. This ensures proper configuration loading and consistent environment behavior across tasks.

--- fbr/config/celery.py
+++ fbr/config/celery.py
@@ -1,6 +1,10 @@
+import os
+
 from celery import Celery
 from celery.schedules import crontab
 from dbt_copilot_python.celery_health_check import healthcheck
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 celery_app = Celery("fbr_celery")
 
