1d5ad577aaa9eec8c2c1c2b6091a2b2f80b353c2
Haresh Kainth <haresh.kainth@businessandtrade.gov.uk>
2024-12-07 00:24:40 +0000

    Refactor environment and Celery configuration setup
    Relocated Django settings initialization in manage.py into the main function for improved execution. Updated Celery configuration to load settings from Django's settings module, ensuring consistency and adaptability in different environments.

--- fbr/manage.py
+++ fbr/manage.py
@@ -1,18 +1,12 @@
-# flake8: noqa
-
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
-import django
-
-django.setup()
-
 
 def main():
     """Run administrative tasks."""
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
     try:
         from django.core.management import execute_from_command_line
