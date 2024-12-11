66735bb1b4d913467945256d460d1e5da2ffb9d3
Haresh Kainth <haresh.kainth@businessandtrade.gov.uk>
2024-12-11 21:26:18 +0000

    chore:remove poetry commands from deployment script
    Simplified the deployment process by removing the use of poetry for running commands. This eliminates unnecessary dependencies and ensures compatibility with environments lacking poetry installed.

--- paas_entrypoint.sh
+++ paas_entrypoint.sh
@@ -3,5 +3,5 @@
 set -ex # Prints each command as it runs. If a command fails, it will halt at that point
 
 cd fbr || exit 1
-poetry run python manage.py migrate --noinput
-poetry run gunicorn config.wsgi --bind "0.0.0.0:$PORT"
+python manage.py migrate --noinput
+gunicorn config.wsgi --bind "0.0.0.0:$PORT"
