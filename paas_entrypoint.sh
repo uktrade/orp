b862768720341d28ad1d0164d2b0b84977a1889e
Haresh Kainth <haresh.kainth@businessandtrade.gov.uk>
2024-12-12 14:17:09 +0000

    remove:Poetry usage from deployment script.
    Replaced Poetry commands with direct Python execution for migrations and server startup. Simplifies the script and removes dependency on Poetry for runtime execution.

--- paas_entrypoint.sh
+++ paas_entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -ex # Prints each command as it runs. If a command fails, it will halt at that point
 
-poetry run python manage.py migrate --noinput
+python manage.py migrate --noinput
 
 # Start the server
-poetry run gunicorn fbr.wsgi:application --bind "0.0.0.0:$PORT"
+gunicorn fbr.wsgi:application --bind "0.0.0.0:$PORT"
